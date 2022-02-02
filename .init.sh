272de7a31800928436ece456712f6563cf2acce6
ini-siro <inisiro@gmail.com>
2022-02-02 19:58:12 +0900

    fix

--- .init.sh
+++ .init.sh
@@ -6,15 +6,17 @@ brew update
 brew upgrade
 brew cleanup
 
+# get this dotfiles repository
+brew install ghq
+ghq get git@github.com:ini-siro/dotfiles.git
+eval "cd $(ghq root)/github.com/ini-siro/dotfiles"
+
+
 # installing tools with brew
 brew bandle
 
 # set fish as the default shell
 sudo sh -c "echo '/opt/homebrew/bin/fish' >> /etc/shells"
 chsh -s /opt/homebrew/bin/fish
-
-# get this dotfiles repository
-ghq get git@github.com:ini-siro/dotfiles.git
-eval "cd $(ghq root)/github.com/ini-siro/dotfiles"
 
 cat hello.txt
